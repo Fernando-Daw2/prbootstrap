5db3cd1133f69af711e118f10d4c6bb3c57ea9d6
Fernando-Daw2 <fpaization@gmail.com>
2016-03-02 09:56:44 +0100

    Contactos y Final

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+          $(".tac").slideUp(0);
+           $(".haz").slideUp(0);
+          
+	$(".distri").click(function() {
+        
+
+        $(".haz").stop();
+          $(".tac").stop();
+             $(".tac").animate({
+
+                 
+                height: "toggle" 
+        
+        },500)
+            
+       
+        s });
+        $(".distri2").click(function() {
+        
+
+        $(".haz").stop();
+          $(".tac").stop();
+             $(".tac").animate({
+
+                 
+                height: "toggle" 
+        
+        },500)
+            
+       
+         });
+          
+          
+ 
+    });
+    
